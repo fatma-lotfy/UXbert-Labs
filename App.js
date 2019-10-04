9bf245b7f4c0cb93af2c728b7ac5e5febc2526bd
FatmaLotfy <47393865+FatmaLotfy@users.noreply.github.com>
2019-10-04 23:57:21 +0200

    UXbertLabs Task

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import RoutingComponent from './SRC/RoutingComponent';
+import Main from './SRC/Main'
+import SplashScreen from './SRC/SplashScreen';
+import APIPage from './SRC/APIPage'
+
+export default class App extends React.Component {
+ render(){
+  return (
+     <RoutingComponent/>
+     //<Main/>
+     //<SplashScreen/>
+     //<APIPage/>
+  );
+ }
+}
+
+
+
